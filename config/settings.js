52f8d3fedd01f482bc543c951d8f7f6ae48594c4
hjm100 <hjm100@126.com>
2018-08-28 18:18:02 +0800

    删除敏感信息

--- config/settings.js
+++ config/settings.js
@@ -3,10 +3,10 @@ module.exports = {
   cdnPath: './', //cdn路径(存放静态资源)
   buildPath:'./dist',//打包路径
   qiniu: {  //七牛云配置
-    ACCESS_KEY: '#############',
-    SECRET_KEY: '#############',
-    bucket: 'bitmain',
-    path: 'visitor-web'
+    ACCESS_KEY: '*******************',
+    SECRET_KEY: '*******************',
+    bucket: '************',
+    path: '******'
   },
   proxy: {    //proxy配置  表示会将/api下的所有请求转发到http://127.0.0.1:3100, 
     "/api": {
