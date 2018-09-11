e9028d614e1bf394007cce215af763779e9b606c
hjm100 <hjm100@126.com>
2018-09-11 18:26:21 +0800

    大转盘动画优化

--- src/views/activity/BagWheel.js
+++ src/views/activity/BagWheel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // 使用外部css文件表
 import '../../styles/views/activity/BagWheel.css';
 import {Toast} from 'antd-mobile';      // 引入加载组件
+import { setInterval } from 'timers';
 class BagWheel extends Component {
   constructor (props) {
     super(props);
@@ -18,26 +19,13 @@ class BagWheel extends Component {
       })
     })
   }
-  // 初始化标签方法
-  wheelItemsEle = (datas)=>{
-    return(
-      <div className="wheel-item" key={datas.id}>
-        <div className="wheel-goods" style={{transform: `rotate(${datas.id*30}deg)`}}>
-          <h3 className="wg-text">{datas.name+datas.id}</h3>
-          <div className="wg-icon">
-            <img src={require('../../assets/hjm100.png')} alt={datas.name}></img>
-          </div>
-        </div>
-      </div>
-    )
-  }
+/******************方法逻辑层*********************/
   getPrize = () =>{
     clearTimeout(this.timer);
     if(this.state.btnEnable){
       this.setState({btnEnable:false})
       //禁止用户连续点击
       React.http.activity.getPrize().then(res => {
-        console.log(res.prize.id)
         this.animation(res.prize.id*30);
         //动画结束后提示用户获取的奖励
         setTimeout(() => {
@@ -51,10 +39,52 @@ class BagWheel extends Component {
   }
   // 获取到奖品后执行动画
   animation = (circle)=>{
+    //周围小球交换显示
+    let loopTime = window.setInterval(()=>{
+      let loopEle = this.refs.loops.querySelectorAll('.loop');
+      for (let i = 0; i < loopEle.length; i++) {
+        if(/(dot1)/.test(loopEle[i].className)){
+         setTimeout(() => {
+            loopEle[i].className = 'loop dot2'
+          }, 100);
+        } else{
+          loopEle[i].className = 'loop dot1'
+        }
+      }
+    },300)
+    setTimeout(()=>{
+      window.clearInterval(loopTime)
+    },6000)
     let wheel_btn = this.refs.wheel_btn,initDeg=0;  
     if(wheel_btn.style.transform)initDeg = wheel_btn.style.transform.replace(/[^0-9]/ig,"")*1
     // 缓冲为6圈
     wheel_btn.style.transform = `rotate(${3600+circle+initDeg-initDeg%360}deg)`
+
+  }
+
+/******************生成标签层*********************/
+
+  // 初始化标签方法
+  wheelItemsEle = (datas)=>{
+    return(
+      <div className="wheel-item" key={datas.id}>
+        <div className="wheel-goods" style={{transform: `rotate(${datas.id*30}deg)`}}>
+          <h3 className="wg-text">{datas.name+datas.id}</h3>
+          <div className="wg-icon">
+            <img src={require('../../assets/hjm100.png')} alt={datas.name}></img>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  setLoopEle = (datas,index)=>{
+    if(index%2 === 0){
+      return(<i className="loop dot2" key={index} style={{transform: `rotate(${index*7.5}deg)`}}></i>)
+    }else{
+      return(<i className="loop dot1" key={index} style={{transform: `rotate(${index*7.5}deg)`}}></i>)
+    } 
+
   }
   //已经生成对应的dom结构
   componentDidMount=()=> {}
@@ -66,7 +96,9 @@ class BagWheel extends Component {
         <div className="title_bar"></div>
         <div className="wheel_box">
           {/*转盘闪环*/}
-          <div className="wheel-loop"></div>
+          <div className="wheel-loop" ref="loops">
+            {[...Array(48)].map(this.setLoopEle)}
+          </div>
           {/*转盘物品*/}
           <div className="wheel-goods_box">{this.state.wheelGoods.map(this.wheelItemsEle)}</div>
           {/*转盘按钮*/}
