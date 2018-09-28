675249c4c28154747f776e7df179cfd67c2c60da
hjm100 <hjm100@126.com>
2018-09-28 14:02:56 +0800

    404页面添加与ui还原

--- src/views/ErrorPage.js
+++ src/views/ErrorPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Button } from 'antd-mobile';
 // 使用外部css文件表
 import '../styles/views/ErrorPage.css'
 class ErrorPage extends Component {
@@ -7,19 +6,13 @@ class ErrorPage extends Component {
     window.location.href = '/';
   }
   render() {
-    let user =  React.store.getState().user
+    // 获取 store数据的方法
+    // let user =  React.store.getState().user
     return (
       <div id="Error">
-        <div className="Error-box">
-          <h3 className="Error-title">404错误！</h3>
-          <p className="Error-dec">抱歉你要找的页面不存在</p>
-          <p>管理员名字：{user.name}</p>
-          <p>管理员电话：{user.phone}</p>
-          <div className="Error-btnbox">
-            <Button className="Error-btn" size="small" type="default" inline onClick={this.goHome}>返回首页</Button>
-            <Button className="Error-btn" size="small" type="default" inline>保证不打死管理员</Button>
-          </div>
-        </div>
+        <div className="Error-img"></div>
+        <div className="Error-txt">房间信息找不到~</div>
+        <div className="Error-btn" onClick={this.goHome}>返回首页</div>
       </div>
     )
   }
