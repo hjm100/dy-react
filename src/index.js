fc671bef661f525832b9eb8d4d066978e610cd5a
hjm100 <hjm100@126.com>
2019-03-21 17:03:39 +0800

    '添加样式重置包'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore} from 'redux'
 import todoApp from './store/reducers'
 import './utils/rem'                                     // 添加rem布局
-import './styles/public/reset.css';                      // 重置标签样式
+import 'hjm-reset.css';                      // 重置标签样式
 import './styles/public/common.css';                     // 公共样式表
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
